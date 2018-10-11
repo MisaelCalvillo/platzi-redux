31aeadbde08869c1e686dcf275eed8b38440d982
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-10-11 11:54:18 -0500

    Actualiza un estado inmutable

--- platzi-video/src/reducers/data.js
+++ platzi-video/src/reducers/data.js
@@ -4,24 +4,14 @@ import { fromJS } from 'immutable';
 const initialState = fromJS({
     entities: schema.entities,
     categories: schema.result.categories,
-    search:[],
+    search:'',
 })
 
 function data(state = initialState, action) {
   switch (action.type) {
     case 'SEARCH_VIDEO': {
       // action.payload.query
-      let results = []
-      const list = state.data.categories[2].playlist;
-      if (action.payload.query) {
-        results = list.filter((item) => {
-          return item.author.includes(action.payload.query)
-        })
-      }
-      return {
-        ...state,
-          search: results,
-      }
+      return state.set('search', action.payload.query)
     }
     default: 
       return state;
